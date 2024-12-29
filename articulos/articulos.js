472003fa892c6a87fe541077bafbb27e81b782bf
paulabolivar <gaby.bolivar05@gmail.com>
2024-12-28 23:24:12 -0300

    hoja nueva

---
+++ articulos/articulos.js
@@ -0,0 +1,11 @@
+console.log("Hola")
+
+let articulos = ["sacapuntas", "lapices"];
+let sacapuntas = ['sAzul','sGris','sRojo'];
+let lapices= ["lAmarillo", "lAzul", "lNegro"]
+
+
+
+function ListaDeArticulos(){
+    document.getElementById("botonLapices").textContent ="LÁPICES"
+}
